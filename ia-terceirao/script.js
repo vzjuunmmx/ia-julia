7faf4e57cba1de533b1cf1f312e1c355d49ec0ed
vzjuunmmx <128061099+vzjuunmmx@users.noreply.github.com>
2024-08-23 11:34:08 -0300

    Add files via upload

---
+++ ia-terceirao/script.js
@@ -0,0 +1,77 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "os animais são inofensivos e a maioria precisa de ajuda. como ajudá los?",
+
+        alternativas: [
+            {
+                texto: "voce pode estar adotando um animalzinho.",
+                afirmacao: "voce é uma otima pessoa, que presa pelos cuidados."
+
+            },
+            {
+                texto: "abrigo de animais.",
+                afirmacao: "tu pensa no cuidado dos animaizinhos"
+            }
+
+        ]
+    },
+    {
+        enunciado: "Plantar árvores frutíferas é essencial para sustentar o meio ambiente e fornecer alimentos frescos aumentando a quantidade de árvores e melhorando a qualidade de vida da população. Qual seria a melhor maneira para começar essa iniciativa?",
+        alternativas: [
+            {
+                texto: "Escolha espécies frutíferas adequadas á sua região, prepare bem o solo e plante em áreas com boa luz solar.",
+                afirmacao: "otimo pensamento"
+            },
+            {
+                texto: "Escolher lugares estratégicos onde podemos aproveitar ao máximo os benefícios que as árvores tem a oferecer. ",
+                afirmacao: "bom pensamento"
+            }
+        ]
+    },
+    {
+        enunciado: "Museus são importantes para ver a trajetória de nossos antepassados,  mas um dia nós seremos a história. O que você quer deixar para a história?",
+
+        alternativas: [
+            {
+                texto: "eu quero deixar um legado!",
+                afirmacao: " otimo pensamento, mostra que voce se importa com sua historia"
+            },
+            {
+                texto: "eu não ligo para a historia, oque importa é o agora, e o futuro!",
+                afirmacao: "bom pensamento"
+            }
+
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa)())
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    atual++;
+    mostraPergunta();
+}
+mostraPergunta();
